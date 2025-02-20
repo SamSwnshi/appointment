f67077baaa6a4ba14133ac3db9cf7b8fdd825b5f
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-02-20 19:34:57 +0530

    appointment routes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ import cookieparser from "cookie-parser";
 import dotenv from "dotenv";
 import config from "./db/config.js";
 
-import doctorRoutes from "./routes/doctor.routes.js"
+import doctorRoutes from "./routes/doctor.routes.js";
+import appointmentRoutes from "./routes/appointment.routes.js";
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(cookieparser())
 
 app.use('/api/doctors',doctorRoutes)
+app.use('/api/appointments',appointmentRoutes)
 
 app.listen(port,()=>{
   console.log(`Server is Running in Port: ${port}`)
